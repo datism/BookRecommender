c9f679885149a71d33d7b7edf53604e3ffee007a
d <dit@memay>
2022-11-24 22:29:05 +0700

    mend

---
+++ src/jobs/recommend.py
@@ -0,0 +1,11 @@
+import json
+from pyspark.sql import SparkSession
+
+def run_job(spark, config):
+    # Run recommend job
+    return
+
+# with open("./src/config.json", "r") as config_file:
+#     config = json.load(config_file)
+# spark = SparkSession.builder.appName(config.get("app_name")).getOrCreate()
+# run_job(spark, config)
