4e90f51afc401f2a648029b8f97e28af078f88f3
quocanhvu16 <quocanhvu16@gmail.com>
2023-01-05 11:18:58 +0700

    Load.py_vqanh_20194228

--- src/shared/load.py
+++ src/shared/load.py
@@ -1,8 +1,25 @@
-def load_movie_metadata(spark):
-    return
+import json
+from pyspark.sql import SparkSession
+
+with open("./src/config.json", "r") as config_file:
+    config = json.load(config_file)
+
+def load_book_metadata(spark):
+    bookFilePath = config.get("DATA_PATH") + "BX-Books.csv"
+    return spark.read.options(inferSchema="true", header="true", delimiter=';').csv(bookFilePath)
 
 def load_user_info(spark):
-    return
+    userFilePath = config.get("DATA_PATH") + "BX-Users.csv"
+    return spark.read.options(inferSchema="true", header="true", delimiter=';').csv(userFilePath)
 
 def load_ratings(spark):
-    return
+    ratingsFilePath = config.get("DATA_PATH") + "BX-Book-Ratings.csv"
+    return spark.read.options(inferSchema="true", header="true", delimiter=';').csv(ratingsFilePath)
+
+# spark = SparkSession.builder.appName(config.get("APP_NAME")).getOrCreate()
+# bookDf = load_book_metadata(spark)
+# bookDf.show(10)
+# userDf = load_user_info(spark)
+# userDf.show(10)
+# ratingDf = load_ratings(spark)
+# ratingDf.show(10)
