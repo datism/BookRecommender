2d1324d5d1c7bf6ff833bd4c702e934d1de49cc1
quocanhvu16 <quocanhvu16@gmail.com>
2023-02-09 18:12:43 +0700

    Update

--- bookrec_be/app.py
+++ bookrec_be/app.py
@@ -21,3 +21,40 @@ def retrive_books(user):
 
     return retrival_output["outputs"]["output_2"][0]
 
+def rank_books(user, books):
+    instances = []
+    for book in books:
+        instances.append({"user_id": user, "isbn": book})
+    ranking_data = json.dumps({"instances": instances})
+
+    response = requests.post('http://localhost:8501/v1/models/ranking_model:predict', data=ranking_data)
+    ranking_output = response.json()
+
+    ranking_predict = ranking_output['predictions']
+    return [ranks[0] for ranks in ranking_predict]
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.get("/<user>/recbooks")
+@cross_origin()
+def getBooks(user):
+    user = int(user) 
+
+    userReadBook = ratingDf[ratingDf['User-ID'] == user]
+
+    if userReadBook.shape[0] == 0:
+        rec_books = bestBookDf["ISBN"][:20]
+        rec_books_df = bookDf[bookDf["ISBN"].isin(rec_books)]
+    else:
+        rec_books = retrive_books(user)
+        rankings = rank_books(user, rec_books)
+
+        idx = numpy.argsort(rankings)
+        sorted_rec_books = numpy.array(rec_books)[idx]
+
+        rec_books_df = bookDf[bookDf["ISBN"].isin(sorted_rec_books)]
+
+    return json.loads(rec_books_df.to_json(orient="records"))
+
